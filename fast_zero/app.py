19e97784d1e85d3813f158aa2034d01e2e5acada
Artur S Farias <artur06farias@gmail.com>
2024-06-25 21:00:53 -0300

    post

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -2,11 +2,18 @@ from http import HTTPStatus
 
 from fastapi import FastAPI
 
-from fast_zero.schemas import Message
+from fast_zero.schemas import Message, UserPublicSchema, UserSchema
 
 app = FastAPI()
+
+database = []
 
 
 @app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
     return {'message': 'Olá Mundo!'}
+
+
+@app.post('/users/', status_code=HTTPStatus.CREATED, response_model=UserPublicSchema)
+def create_user(user: UserSchema):
+    return user
